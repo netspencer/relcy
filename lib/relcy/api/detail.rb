53f39c263b9665ec28e4d2f48b5dedd692495474
Spencer Schoeben <spencer@netspencer.com>
2016-01-28 14:51:23 -0500

    new for v 0.1.4: - load_card method that calls /entity REST endpoint

--- lib/relcy/api/detail.rb
+++ lib/relcy/api/detail.rb
@@ -6,6 +6,10 @@ module Relcy
       def detail(id, options = {})
         @connection.get "detail", { :id => id }.merge(options)
       end
+
+      def load_card(id, options = {})
+        @connection.get "entity", { :lookId => id }.merge(options)
+      end
     end
   end
 end
